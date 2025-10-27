a9c3b803318af28d4d224d61b0163cea9a2c1a77
Eric <eric.gudera@sou.unifeob.edu.br>
2025-10-27 20:58:53 -0300

    Delete lib/server.dart

--- lib/server.dart
+++
@@ -1,6 +0,0 @@
-import'package:projetoinicial_dart/data.dart'
-void main
-var dataAniversario = Data(22, 06, 2006)
-var dataAtual = Data 18, 08, 2025
-print ("Minha data de nascimento é: $[dataAniversario.formatada]");
-print (A data atual é $(dataAtual))
