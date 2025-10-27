a235db40bfa1c70bc418730a2214e466c71a5bf2
lindsaysouza <lindsay.souza@sou.unifeob.edu.br>
2025-10-27 20:50:13 -0300

    Update simulador_service.dart

--- lib/services/simulador_service.dart
+++ lib/services/simulador_service.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+import '../models/leitura_sensor.dart';
+import '../data/sensores_data.dart';
+import 'mysql_service.dart';
+import 'firebase_service.dart';
+
 class SimuladorService {
-  static void simularLeitura() {
-    print('📡 Simulador Sensores - Para implementar pela Lindsay');
-  }
-}
+  Random random = Random();
+  
+  Future<LeituraSensor> gerarLeitura() async 
